0bce56d33308fb88e24c82570ae5984cd22a8201
adrian <adri.latorre@gmail.com>
2021-09-29 15:54:49 +0200

    Add win event

--- src/ts/Screens/WinScreen.ts
+++ src/ts/Screens/WinScreen.ts
@@ -13,7 +13,7 @@ enum WinScreenState {
 
 export class WinScreen extends AbstractScreen {
   music: HTMLAudioElement;
-
+  notificationSent: Boolean;
   killedSprites: HTMLImageElement[] = [];
 
   constructor(game: Game) {
@@ -22,12 +22,12 @@ export class WinScreen extends AbstractScreen {
       new StateObserver(WinScreenState.PlayerAnimation),
       "#202020",
       "YOU ARE NOT A ROBOT 🤖",
-      "",
+      "redirecting to your destination...",
       150
     );
 
     this.music = AudioLoader.load("/sounds/music/ending.mp3", true);
-
+    this.notificationSent = false;
     this.killedSprites[1] = SpriteLoader.load("/sprites/png/killed1.png");
     this.killedSprites[2] = SpriteLoader.load("/sprites/png/killed2.png");
   }
@@ -62,6 +62,13 @@ export class WinScreen extends AbstractScreen {
 
       case WinScreenState.BlackScreen:
         if (this.state.isFirstFrame) this.music.play();
+
+        if (!this.notificationSent) {
+          window.parent.postMessage({
+            'msg': 'win'
+          }, "*");
+          this.notificationSent = true
+        }
 
         if (this.Game.EventManager.isEnterPressed) {
           this.music.pause();
